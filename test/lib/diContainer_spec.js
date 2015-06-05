183663a1b1fac097654fb716cab2b6d7261ec7df
Victor Klapholz <victor.klapholz@gmail.com>
2015-06-06 02:30:08 +0300

    added support for tests

---
+++ test/lib/diContainer_spec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by victor on 06/06/15.
+ */
+
+var expect = require('expect');
+
+describe("diContainer", function() {
+  it('should succeed', function(){
+    expect(true).toBe(true);
+  });
+});
