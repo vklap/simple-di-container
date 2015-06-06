f0a24f8f3e83e3f84d675e6a522f02d305292196
Victor Klapholz <victor.klapholz@gmail.com>
2015-06-06 23:12:10 +0300

    dependencyLeaf_spec

--- test/lib/dependencyLeaf_spec.js
+++ test/lib/dependencyLeaf_spec.js
@@ -1,3 +1,37 @@
 /**
  * Created by victor on 06/06/15.
  */
+
+var expect = require('expect');
+var DependencyLeaf = require('../../lib/dependencyLeaf');
+
+describe("dependencyLeaf", function() {
+  var component;
+  beforeEach(function() {
+    component = new DependencyLeaf('leaf', 'dep');
+  });
+
+  it('should getResolvedInstance', function() {
+    expect(component.getResolvedInstance({})).toEqual('dep');
+  });
+
+  it('should getResolvedInstance', function() {
+    var service = function() { return this; };
+    component = new DependencyLeaf('dep', service);
+
+    expect(component.getResolvedInstance({})).toEqual(service());
+  });
+
+  it('should accept', function() {
+    var visitor = {
+      visitLeaf: function(component) {}
+    };
+
+    var visitLeafSpy = expect.spyOn(visitor, 'visitLeaf');
+
+    component.accept(visitor);
+
+    expect(visitLeafSpy).toHaveBeenCalledWith(component);
+  });
+
+});
